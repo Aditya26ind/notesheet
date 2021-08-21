c9d5f0b15ad91cc17f04de328371089a9a2a3fab
Aditya <adi0699sal@gmail.com>
2021-08-21 13:58:46 +0530

    changed models id to uuid

--- notes/models.py
+++ notes/models.py
@@ -2,10 +2,11 @@
 from userdata.models import user_model
 from django.db import models
 from django.db.models.fields import CharField
+import uuid
 # Create your models here.
 class notes_by_user(models.Model):
     userNotes=models.ForeignKey(user_model,on_delete=models.CASCADE,related_name="notes",blank=False)
-    id = models.UUIDField(primary_key=True, unique=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True)
     note=models.CharField(max_length=500,blank=False)
     def __str__(self):
         return self.note
--- userdata/models.py
+++ userdata/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+import uuid
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 # Create your models here.
 class user_model(AbstractUser):
-    id = models.UUIDField(primary_key=True, unique=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True)
     age=models.CharField(max_length=200,blank=True)
     password=models.CharField(max_length=8,blank=True)
     dob=models.CharField(max_length=200,blank=True)
