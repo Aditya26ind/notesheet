ad9da91ff4c88ab4ad2d464d67a15f89fe11b9a2
Aditya <adi0699sal@gmail.com>
2021-08-24 17:16:18 +0530

    id uuid changes

--- userdata/models.py
+++ userdata/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser, PermissionsMixin
 # Create your models here.
 class user_model(AbstractUser):
     age=models.CharField(max_length=200,blank=True)
-    password=models.CharField(max_length=8,blank=True)
+    password=models.CharField(max_length=800,blank=True)
     dob=models.CharField(max_length=200,blank=True)
     phone=models.CharField(max_length=200,blank=True)
     def __str__(self):
